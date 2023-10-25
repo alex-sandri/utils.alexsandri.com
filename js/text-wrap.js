6dc353107d8d17829dc807d1ac2f6183051a6cbf
Alex Sandri <me@alexsandri.com>
2023-10-25 17:44:41 +0200

    implement first version of text wrap

--- js/text-wrap.js
+++ js/text-wrap.js
@@ -7,12 +7,34 @@
   const container = document.getElementById('prog');
   container.appendChild(textarea);
 
-  // TODO: add input for max line length
   const wrapButton = document.createElement('button');
   wrapButton.innerText = 'Wrap';
 
   wrapButton.addEventListener('click', () => {
-    // TODO
+    const input = textarea.value;
+    const outputLines = [];
+
+    // TODO: add input for max line length
+    const maxLineLength = 80;
+
+    // TODO: for now this is good enough
+    const words = input.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+      const currentWord = words[i];
+
+      const currentLine = outputLines.at(-1) ?? '';
+      const modifiedLine = `${currentLine} ${currentWord}`.trim();
+
+      if (modifiedLine.length > maxLineLength) {
+        outputLines.push(currentWord);
+      } else {
+        outputLines.pop();
+        outputLines.push(modifiedLine);
+      }
+    }
+
+    textarea.value = outputLines.join('\n');
   });
 
   container.appendChild(wrapButton);
